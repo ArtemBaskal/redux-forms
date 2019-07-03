ba0ce4fa470364aa63007c26dacad983afa3cc40
ArtemBaskal <asbaskal@edu.hse.ru>
2019-07-03 13:49:10 +0300

    Form validation and photo adding

--- src/components/Adv.js
+++ src/components/Adv.js
@@ -2,6 +2,10 @@ import React from "react";
 import { Field, reduxForm } from "redux-form";
 
 class Adv extends React.Component {
+  state = {
+    selectedFile: null
+  };
+
   renderError({ error, touched }) {
     if (touched && error) {
       return (
@@ -12,18 +16,73 @@ class Adv extends React.Component {
     }
   }
 
-  renderInput = ({ input, label, isNecessary, maxCharacters, meta }) => {
+  renderHint(isNecessary, { active, pristine, touched, error }) {
+    console.log(error);
+    if (isNecessary && !active && pristine && !touched) {
+      return "Обязательное поле";
+    }
+    if (!active && pristine && touched) {
+      return "Заполните поле";
+    }
+    if (!active && !pristine && touched) {
+      return "Заполнено";
+    }
+    if (error) {
+      return error;
+    }
+  }
+
+  hasErrorClass({ touched, dirty }) {
+    if (dirty) return null;
+    if (touched) return "error";
+  }
+
+  renderInput = ({
+    input,
+    label,
+    isNecessary,
+    maxCharacters,
+    placeholder,
+    meta
+  }) => {
     console.log(meta);
-    const className = ` field ${meta.error && meta.touched ? "error" : ""}`;
     return (
-      <div className={className}>
+      <div className={"field " + this.hasErrorClass(meta)}>
         <label>{label}</label>
-        {isNecessary && <label>Обязательное поле</label>}
-        {maxCharacters && <label>Не более {maxCharacters} символов </label>}
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
+        <label>{this.renderHint(isNecessary, meta)}</label>
+        {maxCharacters && !meta.active && meta.pristine && (
+          <label>Не более {maxCharacters} символов </label>
+        )}
+        <input {...input} placeholder={placeholder} autoComplete="off" />
       </div>
     );
+  };
+
+  fileSelectedHandler = event => {
+    const file = event.target.files[0];
+
+    this.getBase64(file).then(base64 => {
+      localStorage["last"] = base64;
+      localStorage[file.name] = base64;
+      this.setState({
+        selectedFile: base64
+      });
+      console.debug("file stored", base64);
+      console.debug("state", this.state);
+    });
+  };
+
+  getBase64 = file => {
+    return new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onload = () => resolve(reader.result);
+      reader.onerror = error => reject(error);
+      try {
+        reader.readAsDataURL(file);
+      } catch (e) {
+        console.error(e);
+      }
+    });
   };
 
   render() {
@@ -48,9 +107,29 @@ class Adv extends React.Component {
           component={this.renderInput}
           label="Телефон"
           isNecessary
+          placeholder="+7 (___) ___ - __ - __"
         />
         <Field name="city" component={this.renderInput} label="Город" />
-        <button className="ui button">Прикрепить фото</button>
+        <input
+          style={{ display: "none" }}
+          className="ui button"
+          type="file"
+          onChange={this.fileSelectedHandler}
+          ref={fileInput => (this.fileInput = fileInput)}
+        />
+        <img
+          style={{ width: "150px", height: "150px", display: "flex" }}
+          src={this.state.selectedFile}
+        />
+        <button
+          className="ui button"
+          onClick={e => {
+            e.preventDefault();
+            return this.fileInput.click();
+          }}
+        >
+          Выбрать фото
+        </button>
         <button className="ui button primary">Подать</button>
       </form>
     );
@@ -61,12 +140,19 @@ const validate = formValues => {
   const errors = {};
   console.log(formValues);
 
-  if (!formValues.title) {
-    errors.title = "You must enter a title";
+  if (formValues.title && formValues.title.length > 140) {
+    errors.title = "Не более 140 символов";
   }
 
-  if (!formValues.description) {
-    errors.description = "You must enter a description";
+  if (formValues.description && formValues.description.length > 300) {
+    errors.description = "Не более 300 символов";
+  }
+
+  //const regExp = /^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$/;
+  const regExp = /^((\+7|7|8)+([0-9]){10})$/;
+
+  if (formValues.phone && !formValues.phone.toString().match(regExp)) {
+    errors.phone = "Неверный формат";
   }
 
   return errors;
