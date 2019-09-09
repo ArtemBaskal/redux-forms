6e20a89add195029d4ee1793faa4d0024ae0e6de
ArtemBaskal <asbaskal@edu.hse.ru>
2019-09-09 21:56:48 +0300

    Updates

---
+++ precache-manifest.d21026b0f735985faaea7fd5d5f2a68f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62cce7b24408e1ef80b10a43cfb72883",
+    "url": "/testwork/index.html"
+  },
+  {
+    "revision": "abd64706ad4a6a4bc51d",
+    "url": "/testwork/static/css/main.2a5cf4a8.chunk.css"
+  },
+  {
+    "revision": "4c7e07312a730de5defa",
+    "url": "/testwork/static/js/2.9b6942ed.chunk.js"
+  },
+  {
+    "revision": "abd64706ad4a6a4bc51d",
+    "url": "/testwork/static/js/main.9d38a8be.chunk.js"
+  },
+  {
+    "revision": "5aad48fdf79eb0f42513",
+    "url": "/testwork/static/js/runtime~main.3d9b34fd.js"
+  }
+]);
